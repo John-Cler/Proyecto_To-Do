2f057eb6ebbdddc8b207a3fd89a7b5f9f51b5ac9
@eco-intern <git-intern@econotec.com.bo>
2023-09-28 05:21:39 -0400

    Subiendo proyecto

---
+++ GrudBackend/src/test/java/bo/edu/ucb/GrudBackend/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.GrudBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
