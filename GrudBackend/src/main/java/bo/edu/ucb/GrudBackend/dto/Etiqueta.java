a59fd820d769d7e8e3960582948aade626bf0a4b
@eco-intern <git-intern@econotec.com.bo>
2023-09-28 16:02:23 -0400

    Agregando actualizaciones a la Etiqueta dto

--- GrudBackend/src/main/java/bo/edu/ucb/GrudBackend/dto/Etiqueta.java
+++ GrudBackend/src/main/java/bo/edu/ucb/GrudBackend/dto/Etiqueta.java
@@ -1,4 +1,41 @@
 package bo.edu.ucb.GrudBackend.dto;
 
 public class Etiqueta {
+    private Integer etiquetId;
+    private String nombre;
+
+    public Etiqueta(Integer etiquetId, String nombre) {
+        this.etiquetId = etiquetId;
+        this.nombre = nombre;
+    }
+
+    public Etiqueta() {
+
+    }
+
+    public Integer getEtiquetId() {
+        return etiquetId;
+    }
+
+    public void setEtiquetId(Integer etiquetId) {
+        this.etiquetId = etiquetId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "EtiquetaApi{" +
+                "etiquetId=" + getEtiquetId() + "'" +
+                ", nombre='" + getNombre() + "'" +
+                '}';
+    }
+
+
 }
